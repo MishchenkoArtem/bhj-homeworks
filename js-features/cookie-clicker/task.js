c35ed817f2cda96c6932bd014d5cea9a1f6392cf
MishchenkoArtem <84495313+MishchenkoArtem@users.noreply.github.com>
2023-05-02 19:11:45 +0300

    change task.js

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -2,6 +2,7 @@ const cookie = document.querySelector('#cookie');
 const clickerCounter = document.querySelector('#clicker__counter');
 
 cookie.addEventListener('click', () => {
+    console.log('click here');
     if (cookie.style.width='200') {
         cookie.style.width='250'
     } else {
