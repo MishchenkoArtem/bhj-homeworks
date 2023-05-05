fb7ddd41945a0a726089c2c31bb97bedd3e374aa
TheNesh <The.Nesh@yandex.ru>
2023-05-05 19:16:01 +0300

    cookie-clicker

--- js-features/cookie-clicker/task.js
+++ js-features/cookie-clicker/task.js
@@ -3,5 +3,5 @@ const clickerCounter = document.querySelector('#clicker__counter');
 
 cookie.addEventListener('click', () => {
     cookie.width = cookie.width === 200 ? cookie.width = 250 : cookie.width = 200;
-    
+    clickerCounter.textContent = Number(clickerCounter.textContent) + 1;
 });
