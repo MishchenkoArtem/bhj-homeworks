21e021d7f320b60bfb6af9e1ce1f6e72235bec25
TheNesh <The.Nesh@yandex.ru>
2023-05-06 19:02:49 +0300

    countdown

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,6 +1,50 @@
-const timer = document.querySelector('#timer');
-let startTimer = timer.textContent;
+const timer = document.querySelector("#timer");
+let timerValue = timer.textContent.split(":");
 
+// ---------- часы
+let hh = timerValue[0];
+
+// ---------- минуты
+let mm = timerValue[1];
+
+// ---------- секунды
+let ss = timerValue[2];
+
+// ---------- начать выполнение интервала
 const intervalId = setInterval(() => {
+    countdownSs();
+    countdownMm();
+    countdownHh();
     
+    timer.textContent = `${hh < 10 ? `0${hh}` : hh}:${mm < 10 ? `0${mm}` : mm}:${ss < 10 ? `0${ss}` : ss}`;
+
+    if (hh <= 0 && mm <= 0 && ss === 1) {
+        clearInterval(intervalId);
+        alert("Вы победили в конкурсе!");
+    }
 }, 1000);
+
+// ---------- отматать секунды
+const countdownSs = () => {
+    ss -= 1;
+};
+
+// ---------- отматать минуты
+const countdownMm = () => {
+    if (ss === 0) {
+        mm -= 1;
+        ss = 59;
+    }
+};
+
+// ---------- отматать часы
+const countdownHh = () => {
+    if (hh === 0) {
+        return 0;
+    }
+
+    if (mm === 0) {
+        hh -= 1
+        mm = 59;
+    }
+};
