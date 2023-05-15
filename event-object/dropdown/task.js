344e4168a98dadc268c72f714f66ac99005fb72e
TheNesh <The.Nesh@yandex.ru>
2023-05-15 22:59:44 +0300

    change dropdown

--- event-object/dropdown/task.js
+++ event-object/dropdown/task.js
@@ -1,16 +1,12 @@
 const dropdownValue = document.querySelector('.dropdown__value');
 const dropdownList = document.querySelector('.dropdown__list');
 
-document.querySelectorAll('.dropdown__link').forEach(element => { 
-    element.href = '#0';
-});
-
 dropdownValue.addEventListener('click', () => {
-    dropdownList.classList.add('dropdown__list_active');
+    dropdownList.classList.toggle('dropdown__list_active');
 });
-
 
 dropdownList.addEventListener('click', (evt) => {
+    evt.preventDefault();
     let item = evt.target;
     dropdownValue.textContent = item.textContent;
     dropdownList.classList.remove('dropdown__list_active');
