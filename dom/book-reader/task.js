77f56953a50c0b65c7c12fe1c74865e24d5028c9
TheNesh <The.Nesh@yandex.ru>
2023-05-23 23:48:05 +0300

    book

--- dom/book-reader/task.js
+++ dom/book-reader/task.js
@@ -1,10 +1,38 @@
-const fontSize = Array.from(document.querySelectorAll('.font-size'));
+const fontSize = document.querySelectorAll('.font-size');
 const book = document.querySelector('.book');
 
-
-
-fontSize.forEach((element, index) => {
-    element.addEventListener('click', evt => {
+fontSize.forEach((element) => {
+    element.addEventListener('click', (evt) => {
         evt.preventDefault();
+        let item = evt.target.closest('.font-size');
+
+        fontSize.forEach(elem => {
+            elem.classList.remove('font-size_active');
+        });
+
+        item.classList.add('font-size_active');
+
+        if (item.classList.contains('font-size_small')) {
+            smallSize();
+        } else if (item.classList.contains('font-size_big')) {
+            bigSize();
+        } else {
+            regularSize();
+        }    
     });
 });
+
+function smallSize() {
+    book.classList.remove('book_fs-big');
+    book.classList.add('book_fs-small');
+}
+
+function bigSize() {
+    book.classList.remove('book_fs-small');
+    book.classList.add('book_fs-big');
+}
+
+function regularSize() {
+    book.classList.remove('book_fs-big');
+    book.classList.remove('book_fs-small');
+}
